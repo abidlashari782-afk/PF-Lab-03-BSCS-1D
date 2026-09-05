f67e6cb9b01e7fb6451a4c981acf879bbfb30b73
abidlashari782-afk <abidlashari782@gmail.com>
2026-09-05 10:36:10 +0000

    task 03 is done

--- Question_4.c
+++ Question_4.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main(){
+    //calculte the area of traingle
+int base, height, area;
+printf("Enter the value of Height: ");
+scanf("%d", &height);
+printf("Enter The value of Base: ");
+scanf("%d",&base);
+ area = 0.5*base*height;
+ printf("Area of traingle is %d", area);
+}
--- Question_5.c
+++ Question_5.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main (){
+    int x,y;
+    printf("Inpute the value of x: ");
+    scanf("%d",&x);
+    printf("Inpute the value of y: ");
+    scanf("%d",&y);
+    x= x+y;
+    y=x-y;
+    x=x-y;
+    
+    printf("Swaped Values\n");
+    printf("x = %d \n",x);
+    printf("y = %d \n",y);
+}
