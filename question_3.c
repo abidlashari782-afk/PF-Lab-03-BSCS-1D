eba1e9b7a33b47dcdcb5279a58c4f9587862b664
abidlashari782-afk <abidlashari782@gmail.com>
2026-09-05 09:25:18 +0000

    Q3 done

--- question_3.c
+++ question_3.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+int main(){
+    int km,cm;
+    printf("conversion of kilometers into centimeter\n");
+    printf("Enter the distance in Kilometer: ");
+    scanf("%d",&km);
+    cm=km*100000;
+    printf("%dkm is equals to %dcm",km, cm);
+}
